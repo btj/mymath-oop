368d34d17f3366803098712f54cbb455bcb2a166
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-03-03 11:23:15 +0100

    Defensive version

--- mymath-oop/src/MyMath.java
+++ mymath-oop/src/MyMath.java
@@ -5,10 +5,10 @@ public class MyMath {
 	 * Returns the square root of the given number,
 	 * rounded down.
 	 * 
-	 * Contractual programming
+	 * Defensive programming
 	 * 
-	 * @pre The given number is nonnegative.
-	 *     | 0 <= x
+	 * @throws IllegalArgumentException if the given number is negative
+	 *     | x < 0
 	 * @post The result equals the square root of
 	 *       the given number, rounded down.
 	 *     | 0 <= result &&
@@ -16,6 +16,9 @@ public class MyMath {
 	 *     | x < (result + 1) * (result + 1)
 	 */
 	static int sqrt(int x) {
+		if (x < 0)
+			throw new IllegalArgumentException("`x` is negative");
+		
 		int r = 0;
 		while ((r + 1) * (r + 1) <= x)
 			r++;
